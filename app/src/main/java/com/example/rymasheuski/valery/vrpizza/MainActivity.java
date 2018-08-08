783b3882be5596967b07bdc90e644b969f8a7511
Valery Rymasheuski <valery.rymasheuski@gmail.com>
2018-08-08 18:23:28 +0300

    Add drawer, toolbar and ShoppingCart activity

--- app/src/main/java/com/example/rymasheuski/valery/vrpizza/MainActivity.java
+++ app/src/main/java/com/example/rymasheuski/valery/vrpizza/MainActivity.java
@@ -1,24 +1,77 @@
 package com.example.rymasheuski.valery.vrpizza;
 
+import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.rymasheuski.valery.vrpizza.model.Food;
 import com.example.rymasheuski.valery.vrpizza.model.Pizza;
 
-public class MainActivity extends AppCompatActivity  implements FoodTypesFragment.OnFragmentInteractionListener,
-        FoodFragment.OnListFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity  implements FoodFragment.OnListFragmentInteractionListener{
+
+    private FoodTypeFragmentPagerAdapter mFoodTypePagerAdapter;
+    private ViewPager mViewPager;
+    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewPager = findViewById(R.id.pager_food_types);
+        String foodTabs[] = getResources().getStringArray(R.array.food_tabs);
+        mFoodTypePagerAdapter = new FoodTypeFragmentPagerAdapter(getSupportFragmentManager(), foodTabs);
+        mViewPager.setAdapter(mFoodTypePagerAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout_food_types);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        setSupportActionBar(findViewById(R.id.toolbar_main));
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view_main);
+        bottomNavigationView.setOnNavigationItemSelectedListener(menuItem -> {
+
+            switch (menuItem.getItemId()){
+                case R.id.action_food_menu:
+                    startActivity(MainActivity.class);
+                    break;
+
+                case R.id.action_shopping_cart:
+                    startActivity(ShoppingCartActivity.class);
+                    break;
+
+
+
+            }
+            return true;
+
+        });
     }
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
+    private void startActivity(Class<?> intentClass){
+        Intent intent = new Intent(getApplicationContext(), intentClass);
+        startActivity(intent);
     }
 
 
@@ -27,4 +80,68 @@ public class MainActivity extends AppCompatActivity  implements FoodTypesFragmen
     public void onListFragmentInteraction(Food item) {
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_toolbar_shopping_cart:
+                startActivity(ShoppingCartActivity.class);
+                return true;
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+
+    }
+
+    public static class FoodTypeFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        private String foodTabs[];
+
+
+        public FoodTypeFragmentPagerAdapter(FragmentManager fm, String foodTabs[])
+        {
+            super(fm);
+            this.foodTabs = foodTabs;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return FoodFragment.newInstance(position);
+
+
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            if(foodTabs != null){
+                return foodTabs[position];
+            }else{
+                return null;
+            }
+
+
+        }
+
+        @Override
+        public int getCount() {
+            return foodTabs != null ? foodTabs.length : 0;
+        }
+    }
+
 }
